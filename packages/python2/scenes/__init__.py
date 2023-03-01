a0baf0621b888e03475965d7353392e3012b530a
SNI22 <shiyao.ni@mail.mcgill.ca>
2023-03-01 14:01:19 -0500

    Initial commit

---
+++ packages/python2/scenes/__init__.py
@@ -0,0 +1,19 @@
+from .core import Scene
+from .empty_scene import EmptyScene
+from .obstacle_scene import ObstacleScene
+
+
+_scene_classes = {}
+for k, v in locals().items():
+    try:
+        if issubclass(v, Scene):
+            _scene_classes[k] = v
+    except TypeError:
+        pass
+
+
+def get_scene_class(id):
+    if id not in _scene_classes:
+        raise ValueError("Unknown scene class: %s" % id)
+    else:
+        return _scene_classes[id]
